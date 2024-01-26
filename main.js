2e77c0bfbc0a78ae336a6c287e9ad73c84c15597
soliha10 <toxtamurodovagulshodaa@gmail.com>
2024-01-26 22:24:50 +0500

    Practice some methods

--- main.js
+++ main.js
@@ -43,8 +43,9 @@ const first = arr[0][0].toUpperCase();
 const middle = arr[0].slice(1, arr[0].length - 1);
 const last = arr[0][arr[0].length - 1].toUpperCase();
 console.log(first + middle + last);
+
 // let firstEl = arr[0].split("");
 // let firstLetter = firstEl[0].toUpperCase();
 // let lastLetter = firstEl[firstEl.length - 1].toUpperCase();
-// console.log(firstLetter + firstEl.slice(1, arr[0].length - 1) + lastLetter );
-// console.log(firstLetter + lastLetter);
+// let middle = firstEl.slice(1, [firstEl.length - 1]);
+// console.log(firstLetter + middle + lastLetter );
